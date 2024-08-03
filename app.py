b02d63a8f3429e148e34106fe7c5fdec64b5ea45
Jailson Araújo <153243354+jailsonIFRN@users.noreply.github.com>
2024-08-03 13:48:30 +0000

    cardapio.html

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/cardapio")
+def cardapio():
+    return render_template('cardapio.html')
+
