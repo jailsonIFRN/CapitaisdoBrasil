29eeb54afdec25e4783987c59f74c46211c40d82
Jailson Araújo <153243354+jailsonIFRN@users.noreply.github.com>
2024-08-06 20:54:55 +0000

    trabalho final do projeto capitais do brasil - jailson e severino

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def index():
 
 #------ ROTAS DO ARQUIVO da base.html --------
 
-@app.route("/norte")
-def norte():
-    return render_template('norte.html')
+@app.route("/pagina_inicial")
+def pagina_inicial():
+    return render_template('pagina_inicial.html')
 
 @app.route("/nordeste")
 def nordeste():
@@ -29,36 +29,37 @@ def centro_oeste():
     return render_template('centro-oeste.html')
 
 
-# -----------------------------------------------------------
 
-@app.route("/cadastre_se")
-def cadastre_se():
-    return render_template('cadastre_se.html')
-
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro():
-    if request.method == 'POST':
-        # Aqui você pode processar os dados do formulário
-        nome = request.form['nome']
-        senha = request.form['senha']
-        # Adicione código para salvar os dados no banco de dados ou processá-los conforme necessário
-        return redirect(url_for('index'))
-    return render_template('cadastre_se.html')
-
-
-
-# --------------------------------------------------------------------
-@app.route('/cadastre')
-def cadastre():
-    return render_template('cadastrou.html')
-
-@app.route('/base', methods=['GET', 'POST'])
-def base():
-    return redirect(url_for('cadastro'))
-    return render_template('base.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
+# -----------------------------------------------------------
+
+# @app.route("/cadastre_se")
+# def cadastre_se():
+#     return render_template('cadastre_se.html')
+
+# @app.route('/cadastro', methods=['GET', 'POST'])
+# def cadastro():
+#     if request.method == 'POST':
+#         # Aqui você pode processar os dados do formulário
+#         nome = request.form['nome']
+#         senha = request.form['senha']
+#         # Adicione código para salvar os dados no banco de dados ou processá-los conforme necessário
+#         return redirect(url_for('index'))
+#     return render_template('cadastre_se.html')
+
+
+
+# # --------------------------------------------------------------------
+# @app.route('/cadastre')
+# def cadastre():
+#     return render_template('cadastrou.html')
+
+# @app.route('/base', methods=['GET', 'POST'])
+# def base():
+#     return redirect(url_for('cadastro'))
+#     return render_template('base.html')
