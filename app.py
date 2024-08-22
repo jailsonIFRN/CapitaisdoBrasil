0fcab1337cedb1e02d7be82dc94c7e3b49688b47
Jailson Araújo <153243354+jailsonIFRN@users.noreply.github.com>
2024-08-22 22:34:50 +0000

    1

--- app.py
+++ app.py
@@ -61,31 +61,3 @@ def buscar():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# -----------------------------------------------------------
-
-# @app.route("/cadastre_se")
-# def cadastre_se():
-#     return render_template('cadastre_se.html')
-
-# @app.route('/cadastro', methods=['GET', 'POST'])
-# def cadastro():
-#     if request.method == 'POST':
-#         # Aqui você pode processar os dados do formulário
-#         nome = request.form['nome']
-#         senha = request.form['senha']
-#         # Adicione código para salvar os dados no banco de dados ou processá-los conforme necessário
-#         return redirect(url_for('index'))
-#     return render_template('cadastre_se.html')
-
-
-
-# # --------------------------------------------------------------------
-# @app.route('/cadastre')
-# def cadastre():
-#     return render_template('cadastrou.html')
-
-# @app.route('/base', methods=['GET', 'POST'])
-# def base():
-#     return redirect(url_for('cadastro'))
-#     return render_template('base.html')
